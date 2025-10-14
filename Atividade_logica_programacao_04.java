ee597a3c30f5c8a1525611423f6028f5b4102aa1
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2025-10-14 16:33:41 -0300

    Add files via upload

--- Atividade_logica_programacao_04.java
+++ Atividade_logica_programacao_04.java
@@ -1,8 +1,23 @@
+import java.util.Random;
 import java.util.Scanner;
 public class Atividade_logica_programacao_04 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+        Random random=new Random();
+        double totalpecas=200;
+        double defeitos=0;
+        double resultado=0;
+        for (int numero=1;numero<=200;numero++){
+            System.out.println("A peça é defeituosa? (sim/não)");
+            String defeituosa= sc.nextLine();
+            if (defeituosa.equalsIgnoreCase("sim")){
+                defeitos++;
+            }
+            else {
+                System.out.println("Peça ideal.");
+            }
         }
+        resultado=(defeitos*100)/totalpecas;
+        System.out.println("A porcentagem de defeitos do lote é:" +resultado+"%");
     }
 }
