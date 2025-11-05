2a276a91ffc1e186e1ef59d14ecfd2d809c50c4c
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2025-11-05 16:10:47 -0300

    Add files via upload

---
+++ Ativ21_Exercicios_de_ArrayList/ArrayList_05.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Produto {
+    String nome;
+
+    public class ArrayList_05 {
+        public static void main(String[] args) {
+        }
+    }
+}
---
+++ Ativ21_Exercicios_de_ArrayList/ArrayList_06.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ArrayList_06 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Digite os nomes e digite FIM para parar:");
+        String contato=sc.nextLine();
+        ArrayList<contato>
+
+    }
+}
